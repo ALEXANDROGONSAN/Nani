12ea3d7bd1cf40d00d2f223b6ecc157fa1519709
ALEXANDROGONSAN <38570195+ALEXANDROGONSAN@users.noreply.github.com>
2019-03-24 16:50:06 -0400

    Creates
     index.php
     index.db

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+	if ( isset($_POST['text']) {
+		if ( strtolower($_POST['text']) == 'tcc' || 
+				 strtolower($_POST['text']) == 'libpng' || 
+				 strtolower($_POST['text']) == 'cg'
+				){
+			system('');
+		}
+	}
+
+ ?>
+<form action="#" method="post">
+	<input type="text" name="text">
+	<input type="submit">
+</form>
